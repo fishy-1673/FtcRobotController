c7b089ba521c03ec13814c58ddc0e2f548f934fb
finna <finnangkelly@gmail.com>
2026-01-05 21:31:22 -0900

    Indexer fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/Indexer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/Indexer.java
@@ -2,14 +2,14 @@
 package org.firstinspires.ftc.teamcode.systems;
 
 
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class Indexer {
     private Servo Index;
     private Servo Ejector;
-    private Double[] InPos = {0.45,0.0,0.90,};
+    private Double[] EPos = {0.45,0.0,0.90,};
+    private Double[] InPos = {};
     public Indexer(HardwareMap hardwareMap) {//maybye define a local hardware map with  this.hardwareMap = hardwareMap;  ?
         Index = hardwareMap.get(Servo.class, "Is");
         Ejector = hardwareMap.get(Servo.class, "Es");
@@ -17,6 +17,9 @@ public class Indexer {
 
 
     public void Index(int pos) {
+        if (pos<4&&pos>=0) Index.setPosition(EPos[pos]);
+    }
+    public void Load(int pos) {
         if (pos<4&&pos>=0) Index.setPosition(InPos[pos]);
     }
     public void Eject(boolean i){
